b261de3bd74cc5a09f98e8097a770bf0c9990def
Seth Harmon <harmonsc21@gcc.edu>
2026-02-05 12:07:10 -0500

    adding CSVWriterMock

---
+++ src/CSVWriterMock.java
@@ -0,0 +1,17 @@
+import com.opencsv.CSVWriter;
+
+import java.io.Writer;
+import java.util.ArrayList;
+
+public class CSVWriterMock extends CSVWriter {
+    public ArrayList<String[]> lines;
+    public CSVWriterMock(Writer writer) {
+        super(writer);
+        lines = new ArrayList<>();
+    }
+
+    @Override
+    public void writeNext(String[] nextLine) {
+        lines.add(nextLine);
+    }
+}
--- src/GameOverPanel.java
+++ src/GameOverPanel.java
@@ -69,25 +69,29 @@ public class GameOverPanel extends JPanel {
      * Sets the game results, updates the UI, and saves results to the log file (if human was playing)
      */
     // TODO: refactor this method
-    public void setGameResults(GameResult result){
+    public void setGameResults(GameResult result, CSVWriter writer){
         this.gameResult = result;
-
+        // refactor getting the text value to return to ensure correct output
         answerTxt.setText(getAnswerText(result));
         numGuessesTxt.setText(getNumGuessesText(result));
-
+        // refactor to not write to a file but return what it would have written
         if(result.humanWasPlaying){
             // write stats to file
-            try(CSVWriter writer = new CSVWriter(new FileWriter(StatsFile.FILENAME, true))) {
 
-                String [] record = new String[2];
-                record[0] = LocalDateTime.now().toString();
-                record[1] = Integer.toString(result.numGuesses);
+            String [] record = new String[2];
+            record[0] = LocalDateTime.now().toString();
+            record[1] = Integer.toString(result.numGuesses);
 
-                writer.writeNext(record);
-            } catch (IOException e) {
-                // NOTE: In a full implementation, we would log this error and possibly alert the user
-                // NOTE: For this project, you do not need unit tests for handling this exception.
-            }
+            writer.writeNext(record);
+        }
+    }
+
+    public void setGameResults(GameResult result) {
+        try(CSVWriter writer = new CSVWriter(new FileWriter(StatsFile.FILENAME, true))) {
+            setGameResults(result, writer);
+        } catch (IOException e) {
+            // NOTE: In a full implementation, we would log this error and possibly alert the user
+            // NOTE: For this project, you do not need unit tests for handling this exception.
         }
     }
 
