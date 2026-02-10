8d51178bdf8a5c9c8a159194bb0e4fc4e499cd81
Graybill <graybillkb20@gcc.edu>
2026-02-10 10:14:55 -0500

    Updated game over writing tests

--- src/GameOverWriter.java
+++ src/GameOverWriter.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 import com.opencsv.CSVWriter;
 
 public class GameOverWriter {
-    public static void writeGameResults(GameResult result, CSVWriter writer) {
+    public static void writeGameResults(GameResult result, CSVWriter writer, LocalDateTime date) {
         // write stats to file
 
         String [] record = new String[2];
-        record[0] = LocalDateTime.now().toString();
+        record[0] = date.toString();
         record[1] = Integer.toString(result.numGuesses);
 
         writer.writeNext(record);
@@ -16,7 +16,7 @@ public class GameOverWriter {
 
     public static void writeGameResults(GameResult result) {
         try(CSVWriter writer = new CSVWriter(new FileWriter(StatsFile.FILENAME, true))) {
-            writeGameResults(result, writer);
+            writeGameResults(result, writer, LocalDateTime.now());
         } catch (IOException e) {
             // NOTE: In a full implementation, we would log this error and possibly alert the user
             // NOTE: For this project, you do not need unit tests for handling this exception.
--- src/GameOverWriterTest.java
+++ src/GameOverWriterTest.java
@@ -2,18 +2,37 @@ import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameOverWriterTest {
 
     @Test
     void setGameResults() {
-        GameOverPanel gop = new GameOverPanel(new JPanel());
         WriterMock writer = new WriterMock();
         CSVWriterMock csv = new CSVWriterMock(writer);
         GameResult gr = new GameResult(true, 500, 1);
-        GameOverWriter.writeGameResults(gr, csv);
+        LocalDateTime date = LocalDateTime.now();
+        GameOverWriter.writeGameResults(gr, csv, date);
         assertEquals(1, csv.lines.size());
         assertEquals("1", csv.lines.getFirst()[1]);
+        assertEquals(date.toString(), csv.lines.getFirst()[0]);
+    }
+
+    @Test
+    void setMultipleGameResults() {
+        WriterMock writer = new WriterMock();
+        CSVWriterMock csv = new CSVWriterMock(writer);
+        GameResult gr = new GameResult(true, 480, 1);
+        LocalDateTime firstTime = LocalDateTime.now();
+        GameOverWriter.writeGameResults(gr, csv, firstTime);
+        gr = new GameResult(true, 89, 4);
+        LocalDateTime secondTime = LocalDateTime.now();
+        GameOverWriter.writeGameResults(gr, csv, secondTime);
+        assertEquals(2, csv.lines.size());
+        assertEquals("1", csv.lines.getFirst()[1]);
+        assertEquals(firstTime.toString(), csv.lines.getFirst()[0]);
+        assertEquals(secondTime.toString(), csv.lines.getLast()[0]);
     }
 }
